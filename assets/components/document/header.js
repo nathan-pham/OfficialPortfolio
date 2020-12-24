82afd7ad6a94b6c519b5d64b58b7b2d0463970d1
Nathan Pham <nathanpham.me@gmail.com>
2020-12-23 20:46:44 -0800

    active header links

--- assets/components/document/header.js
+++ assets/components/document/header.js
@@ -1,5 +1,30 @@
+import { useRouter } from "next/router"
 import Link from "next/link"
 import Section from "./section"
+
+
+const DefaultLink = () => {
+    const router = useRouter()
+
+    return (
+        <>
+            <Link href="/">
+                <a className={ router.pathname == "/" ? "active" : "" }>Work</a>
+            </Link>
+            <Link href="/about">
+                <a className={ router.pathname == "/about" ? "active" : "" }>About</a>
+            </Link>
+        </>
+    )
+}
+
+const BackLink = () => {
+    return (
+        <Link href="/">
+            <a>Back</a>
+        </Link>
+    )
+}
 
 const Header = ({ type }) => {
     return (
@@ -8,21 +33,8 @@ const Header = ({ type }) => {
                 <nav className="nav-links">
                     {
                         type == "default"
-                            ? (
-                                <>
-                                    <Link href="/">
-                                        <a className="active">Work</a>
-                                    </Link>
-                                    <Link href="/about">
-                                        <a>About</a>
-                                    </Link>
-                                </>
-                            )
-                            : (
-                                <Link href="/">
-                                    <a>Back</a>
-                                </Link>
-                            )
+                            ? <DefaultLink />
+                            : <BackLink />
                     }
                     
                 </nav>
