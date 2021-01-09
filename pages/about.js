243431aaea0edb96cd0ee2dc74175da0170d0006
Nathan Pham <nathanpham.me@gmail.com>
2021-01-09 11:59:05 -0800

    lazy loading

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ const Home = ({ posts }) => {
     <Root title="About">
       <Section>
         <figure className="hero">
-          <img src="/images/hero.jpeg" alt="Speaking at a YLA session" />
+          <img src="/images/hero.jpeg" alt="Speaking at a YLA session" loading="lazy" />
           <figcaption>
             Speaking at a YLA Session
           </figcaption>
