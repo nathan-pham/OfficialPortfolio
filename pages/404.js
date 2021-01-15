78b9ea041cb31f29dba95957ffb018e749afc7a6
Nathan Pham <nathanpham.me@gmail.com>
2021-01-14 22:44:43 -0800

    margin & font

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ const Error = () => {
         <Root title="Error" no_header={ true } no_footer={ true }>
             <main className="flex align-center justify-center error-wrapper">
                 <div className="error-content">
-                    <h1 className="big title title-font">¯\_(ツ)_/¯</h1>
+                    <h1 className="big title">¯\_(ツ)_/¯</h1>
                     <h2 className="big title title-font">Nothing here</h2>
                     <p>Well I don't see anything. Let's head back home.</p>
                     <a href="/" className="title-font">
