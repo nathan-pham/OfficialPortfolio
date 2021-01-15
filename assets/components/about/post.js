b0d4d740c3822eae0363a8bc41c8a35513d20be0
Nathan Pham <nathanpham.me@gmail.com>
2021-01-14 22:37:30 -0800

    key errors

--- assets/components/about/post.js
+++ assets/components/about/post.js
@@ -1,6 +1,6 @@
 const Post = ({ url, cover_image, title, description }) => {
     return (
-        <a href={ url } className="blog-post flex direction-column">
+        <a href={ url } className="blog-post flex direction-column" key={ url }>
             <article>
                 <img src={ cover_image } />
                 <h2 className="big subtitle title-font">{ title }</h2>
